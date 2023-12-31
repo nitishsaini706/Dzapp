82571f16a5f7e33c2d0d2a91a5f84ca89c86678b
nitishsaini706 <nitishsaini706@gmail.com>
2023-12-31 11:22:45 +0530

    fix

--- frontend/src/components/Component.tsx
+++ frontend/src/components/Component.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { currencyList, convert, cryptoList } from "../handlers/apiCalls";
 
 
-
 interface Crypto {
   id: string;
   name: string;
@@ -18,6 +17,7 @@ const Converter = () => {
   const [result, setResult] = useState("");
   const [show, setShow] = useState(false);
   const [miss, setMiss] = useState("");
+  const [loading,setLoading]=useState(false);
 
   useEffect(() => {
     // Fetch currency list
@@ -55,23 +55,53 @@ const Converter = () => {
       crypto,
       amount,
     };
-
+    setLoading(true);
     convert(query)
-      .then((res) => res.success && setResult(res.data))
+      .then((res) => res.success && setResult(res.data.data))
       .catch((err) => {
         console.error("Error in converting:", err);
         throw err;
       });
+    setLoading(false);
   };
 
   return (
     <>
-      {/* {show && (
-        <div style={{ zIndex: "1000000000", borderRadius: "30px", border: "2px solid white" }}>
-          <h1 style={{ fontSize: "30px" }}>{miss}</h1>
-          <button style={{ height: "20px", fontSize: "20px", border: "2px solid white" }} onClick={() => setShow(false)}>OK</button>
-        </div>
-      )} */}
+    {loading && <div className="loader"></div>}
+    {
+      show && 
+    
+		<div
+			onClick={()=>setShow(false)}
+			style={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<div
+				style={{
+					background: "white",
+					height: 50,
+					width: 240,
+					margin: "auto",
+					padding: "2%",
+					border: "2px solid #000",
+					borderRadius: "10px",
+					boxShadow: "2px solid black",
+				}}
+			>
+				{miss}
+      <button style={{backgroundColor:'red',color:'white',marginLeft:"10px",width:"20px",height:"25px"}} onClick={()=>setShow(false)}></button>
+			</div>
+		</div>
+    
+      }
       <div>
         <h1>Crypto Converter</h1>
       </div>
@@ -80,7 +110,7 @@ const Converter = () => {
           <div>
 
             <label style={{ marginRight: "5px" }}>Currency *</label>
-            <select style={{ bottom: 0, maxWidth: "100px" }} onChange={(e) => { setCurrency(e.target.value) }} name="currency">
+            <select style={{ bottom: "0", maxWidth: "100px" }} onChange={(e) => { setCurrency(e.target.value) }} name="currency">
               <option value="usd" style={{ padding: "10px" }}>USD</option>
               {Object.entries(currencies).map(([currencyCode, currencyName]) => (
                 <option style={{ maxWidth: "100px", padding: "5px" }} key={currencyCode} value={currencyCode}>
@@ -94,7 +124,7 @@ const Converter = () => {
           <div>
 
             <label style={{ marginRight: "5px" }}>Crypto *</label>
-            <select style={{ bottom: 0, maxWidth: "100px", border: { show } ? "2px solid red" : "none" }} onChange={(e) => { setShow(false); setCrypto(e.target.value) }}>
+            <select style={{ bottom: "0", maxWidth: "100px", }} onChange={(e) => { setCrypto(e.target.value) }}>
               <option value="" disabled>Select Crypto</option>
               {cryptos.map((item) => (
                 <option style={{ maxWidth: "100px" }} key={item.id} value={item.id}>
@@ -103,11 +133,9 @@ const Converter = () => {
               ))}
             </select>
           </div>
-          {show && <label style={{ color: "red", fontSize: "10px" }}>required *</label>}
-
         </div>
       </div>
-      <input type="number" style={{ border: { show } ? "2px solid red" : "none" }} name="amount" value={amount} placeholder="Amount" onChange={(e) => { setShow(false); setAmount(e.target.value) }} />
+      <input type="number" name="amount" value={amount} placeholder="Amount" onChange={(e) => { setAmount(e.target.value) }} />
       <button style={{ border: "2px solid white", marginLeft: "20px" }} onClick={convertCurrency}>Convert</button>
       {result != "" && <div><label>{result}</label></div>}
     </>
